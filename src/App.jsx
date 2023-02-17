9c71cdfd8674b5a0a175016e2bc6c4a4b827afcb
mukhamed-Ali <mailsmorales21@mail.ru>
2023-02-18 02:34:31 +0300

    add components and Preparation tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import { Route, Routes } from "react-router";
 import { HomePage } from "./Pages/HomePage";
+import { Layout } from "./components/Layout/Layout";
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
