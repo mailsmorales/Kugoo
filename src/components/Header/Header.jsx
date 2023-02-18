f6e1bc64959c481472ad53c6a5f15afd14378774
mukhamed-Ali <mailsmorales21@mail.ru>
2023-02-18 17:18:44 +0300

    add component headertop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { HeaderTop } from "./components/HeaderTop";
+import { Navbar } from "./components/Navbar";
 
 export const Header = () => {
   return (
-    <div className='custom-container'>Header</div>
-  )
-}
+    <div className="custom-container">
+      <HeaderTop />
+      <Navbar />
+    </div>
+  );
+};
---
+++ src/components/Header/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className=" mt-10">
+      <h1>KUGOO</h1>
+    </div>
+  );
+};
