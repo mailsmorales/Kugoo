f5860f34887b07c4e69efc180a9e492c7456f94f
mukhamed-Ali <mailsmorales21@mail.ru>
2023-03-04 23:56:34 +0300

    made a contact popup

--- src/components/Header/components/CatalogItem.jsx
+++ src/components/Header/components/CatalogItem.jsx
@@ -17,69 +17,67 @@ export const CatalogItem = () => {
         <Icons.Line />
         <button className="font-medium text-white">Каталог</button>
       </div>
-      {
-        <div
-          className={`absolute left-0 top-13 bg-white px-3 py-3 shadow-xl rounded-md flex duration-500 ${
-            showCatalog
-              ? "translate-y-0 opacity-1 visible"
-              : "-translate-y-1 opacity-0 invisible"
-          }`}
-        >
-          <div className="bg-bg rounded-md px-6 py-5 mr-10">
+      <div
+        className={`absolute left-0 top-13 bg-white px-3 py-3 shadow-xl rounded-md flex duration-500 ${
+          showCatalog
+            ? "translate-y-0 opacity-1 visible"
+            : "-translate-y-1 opacity-0 invisible"
+        }`}
+      >
+        <div className="bg-bg rounded-md px-6 py-5 mr-10">
+          <ul>
+            {catalogItems.map(({ Icon, ...item }, index) => (
+              <li
+                onClick={() => setSelectedItem(index)}
+                key={item.title}
+                className={clsx(
+                  "flex items-center cursor-pointer gap-2 mb-4 font-medium hover:text-purple",
+                  selectedItem === index ? "text-purple " : ""
+                )}
+              >
+                <div>
+                  <Icon />
+                </div>
+                <span>{item.title}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="py-5 mr-14">
+          <div>
+            <h4 className="font-medium mb-4">Особенности</h4>
             <ul>
-              {catalogItems.map(({ Icon, ...item }, index) => (
+              {catalogItems[selectedItem].subItems.map((item, index) => (
                 <li
-                  onClick={() => setSelectedItem(index)}
-                  key={item.title}
+                  onClick={() => setSelectedItems(index)}
+                  key={item.titleType}
                   className={clsx(
-                    "flex items-center cursor-pointer gap-2 mb-4 font-medium hover:text-purple",
-                    selectedItem === index ? "text-purple " : ""
+                    "text-sm mb-2 cursor-pointer hover:text-purple",
+                    selectedItems === index ? "text-purple" : "text-gray-600"
                   )}
                 >
-                  <div>
-                    <Icon />
-                  </div>
-                  <span>{item.title}</span>
+                  {item.titleType}
                 </li>
               ))}
             </ul>
           </div>
-          <div className="py-5 mr-14">
-            <div>
-              <h4 className="font-medium mb-4">Особенности</h4>
-              <ul>
-                {catalogItems[selectedItem].subItems.map((item, index) => (
-                  <li
-                    onClick={() => setSelectedItems(index)}
-                    key={item.titleType}
-                    className={clsx(
-                      "text-sm mb-2 cursor-pointer hover:text-purple",
-                      selectedItems === index ? "text-purple" : "text-gray-600"
-                    )}
-                  >
-                    {item.titleType}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="py-5 mr-14">
-            <h4 className="font-medium">Для кого</h4>
-            <ul>
-              {catalogItems[selectedItem].subItems[selectedItems].subItems.map(
-                (item) => (
-                  <li
-                    key={item.title}
-                    className="text-sm text-gray-600 mb-2 whitespace-nowrap cursor-pointer hover:text-purple"
-                  >
-                    {item.title}
-                  </li>
-                )
-              )}
-            </ul>
-          </div>
         </div>
-      }
+        <div className="py-5 mr-14">
+          <h4 className="font-medium">Для кого</h4>
+          <ul>
+            {catalogItems[selectedItem].subItems[selectedItems].subItems.map(
+              (item) => (
+                <li
+                  key={item.title}
+                  className="text-sm text-gray-600 mb-2 whitespace-nowrap cursor-pointer hover:text-purple"
+                >
+                  {item.title}
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Header/components/HeaderTop.jsx
+++ src/components/Header/components/HeaderTop.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Icons from "../../../assets/image/icons";
 
 export const HeaderTop = () => {
+  const [showContact, setShowContact] = useState(false);
+  const contact = [
+    {
+      title: "Сервисный центр",
+      number: "+ 7 (499) 350 76 92",
+    },
+    {
+      title: "Оптовый отдел",
+      number: "+7 (499) 281-64-52",
+      time: "пн-сб 10:00 - 19:00",
+    },
+    {
+      title: "Отдел рекламаций и претензий",
+      number: "+7 (499) 350-76-92",
+      time: "ср-вс с 10:00 до 19:00",
+    },
+  ];
+
   return (
     <div className="flex justify-between">
       <div className="text-xs flex-row flex justify-center items-center gap-7 text-gray-600">
@@ -10,17 +28,46 @@ export const HeaderTop = () => {
         <Link className="hover:text-black">Сотрудничество</Link>
         <Link className="hover:text-black">Заказать звонок</Link>
         <div className="flex gap-2 cursor-pointer">
-          <Icons.Viber/>
-          <Icons.Whatsapp/>
-          <Icons.Telegram/>
+          <Icons.Viber />
+          <Icons.Whatsapp />
+          <Icons.Telegram />
         </div>
       </div>
       <div>
-        <div className="flex items-center justify-center gap-2 cursor-pointer">
-          <span className="text-xs font-medium text-black">
+        <div className="flex items-center justify-center gap-2 relative">
+          <a
+            className="text-xs font-medium text-black cursor-pointer"
+            href="tel:+78005055461"
+          >
             +7 (800) 505-54-61
-          </span>
-          <Icons.Plus className=" hover:fill-gray-200" />
+          </a>
+          <div onClick={() => setShowContact(!showContact)}>
+            <Icons.Plus className=" hover:fill-gray-200 cursor-pointer" />
+          </div>
+          <div
+            className={`absolute w-60 right-0 top-5 bg-bg py-4 shadow-xl rounded-md flex duration-500 ${
+              showContact
+                ? "translate-y-0 opacity-1 visible"
+                : "-translate-y-1 opacity-0 invisible"
+            }`}
+          >
+            <div className="w-full">
+              <ul>
+                {contact.map((item) => (
+                  <li
+                    key={item.title}
+                    className="px-5 py-3 first:pt-0 last:pb-0 border-b border-gray-200 last:border-b-0 last:mb-0"
+                  >
+                    <span className="text-xs text-gray-600">{item.title}</span>
+                    <h3 className="font-medium cursor-pointer">
+                      {item.number}
+                    </h3>
+                    <span className="text-xs">{item.time}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
